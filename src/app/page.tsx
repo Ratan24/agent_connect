855a90d58f0069ded699b7e8547143b8d7a46de4
Ratan-ce <76002894+Ratan-ce@users.noreply.github.com>
2025-08-23 10:16:29 -0400

    03: authentication-setup

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,70 @@
+"use client";
+
+import { Input } from "@/components/ui/input";
 import { Button } from "@/components/ui/button";
+import { useState } from "react";
+import { authClient } from "@/lib/auth-client";
+
 export default function Home() {
+
+  const {data: session} = authClient.useSession() 
+
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const onSubmit = () => {
+    authClient.signUp.email({
+      email,
+      password,
+      name,
+    }, {
+      onError: (error: any) => {
+        window.alert(error.message);
+      },
+      onSuccess: () => {
+        window.alert("User created successfully");
+      }
+    });
+  };
+
+  const onLogin = () => {
+    authClient.signIn.email({
+      email,
+      password,
+    }, {
+      onError: (error: any) => {
+        window.alert(error.message);
+      },
+      onSuccess: () => {
+        window.alert("User Logged in successfully");
+      }
+    });
+  };
+
+  if (session) {
+    return (<div className="flex flex-col items-center justify-center h-screen">
+      <p>Logged in {session.user?.name}</p>
+      <Button onClick={() => authClient.signOut()}>Sign Out</Button>
+      </div>
+    );
+  }
+
   return (
-    <Button variant="destructive">
-      Click me
-    </Button>
+    <div>
+      <div className="flex flex-col items-center justify-center h-screen">
+        <p>Login</p>
+        <Input type="email" placeholder="Email" value={email} onChange={(e) => setEmail(e.target.value)} />
+        <Input type="password" placeholder="Password" value={password} onChange={(e) => setPassword(e.target.value)} />
+        <Button onClick={onLogin}>Login</Button>
+      </div>
+      <div>
+      <Input type="text" placeholder="Name" value={name} onChange={(e) => setName(e.target.value)} />
+      <Input type="email" placeholder="Email" value={email} onChange={(e) => setEmail(e.target.value)} />
+      <Input type="password" placeholder="Password" value={password} onChange={(e) => setPassword(e.target.value)} />
+      <Button onClick={onSubmit}>Create User</Button>
+      </div>
+    </div>
   )
  
 }
