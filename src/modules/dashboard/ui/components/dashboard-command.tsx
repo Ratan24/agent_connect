654175e6d9c520796ef1dfe4b1415cd85dd52162
Ratan-ce <76002894+Ratan-ce@users.noreply.github.com>
2025-08-25 17:09:18 -0400

    07: dashboard-navbar

---
+++ src/modules/dashboard/ui/components/dashboard-command.tsx
@@ -0,0 +1,19 @@
+import {CommandDialog, CommandInput, CommandList, CommandEmpty, CommandItem} from "@/components/ui/command";
+import { Dispatch, SetStateAction } from "react";
+
+interface Props {
+    open:boolean;
+    setOpen: Dispatch<SetStateAction<boolean>>;
+}
+
+
+export const DashboardCommand = ({open, setOpen}: Props) => {
+    return (
+        <CommandDialog open={open} onOpenChange={setOpen}>
+            <CommandInput placeholder="Find a meeting or agent..."/>
+            <CommandList>
+                <CommandItem>No results found.</CommandItem>
+            </CommandList>
+        </CommandDialog>
+    )
+}
