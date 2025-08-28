1e743abcdcb6f2ca766c81e3a95280e702cfb144
Ratan-ce <76002894+Ratan-ce@users.noreply.github.com>
2025-08-28 18:26:25 -0400

    13: agents-filters

--- src/modules/agents/ui/views/agents-view.tsx
+++ src/modules/agents/ui/views/agents-view.tsx
@@ -7,15 +7,30 @@ import { ErrorState } from "@/components/error-state";
 import { DataTable } from "../components/data-table";
 import { columns } from "../components/columns";
 import { EmptyState } from "@/components/empty-state";
+import { useAgentsFilters } from "../../hooks/use-agents-filters";
+import { DEFAULT_PAGE_SIZE } from "@/constants";
+import { DataPagination } from "../components/data-pagination";
 
 export const AgentsView = () => {
+    const [filters, setFilters] = useAgentsFilters();
     const trpc = useTRPC();
-    const {data } = useSuspenseQuery(trpc.agents.getMany.queryOptions());
+    const {data } = useSuspenseQuery(trpc.agents.getMany.queryOptions({
+        pagination: {
+            search: filters.search || undefined,
+            page: filters.page,
+            pageSize: DEFAULT_PAGE_SIZE
+        }
+    }));
 
     return (
         <div className = "flex-1 flex flex-col gap-y-4 md:px-8 px-4 pb-4">
-            <DataTable data={data} columns={columns} />
-            {data.length === 0 && 
+            <DataTable data={data.items} columns={columns} />
+            <DataPagination
+            page={filters.page}
+            totalPages={data.totalPages}
+            onPageChange={(page) => setFilters({page})}
+            />
+            {data.items.length === 0 && 
             <EmptyState title="Create your first Agent" description="Create an agent to join your meetings. Each agent will follow your instructions and can interact with participants during the call" />}
         </div>
     )
