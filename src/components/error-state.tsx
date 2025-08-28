46136a681b0672ffc8c462412815789feb2ebb6e
Ratan-ce <76002894+Ratan-ce@users.noreply.github.com>
2025-08-28 16:04:23 -0400

    12: agents-table

--- src/components/error-state.tsx
+++ src/components/error-state.tsx
@@ -10,7 +10,7 @@ export const ErrorState = ({title, description}: Props) => {
         <div className="flex flex-col items-center justify-center h-screen">
             <div className="flex flex-col items-center justify-center gap-y-6 bg-background rounded-lg p-10 shadow-sm">
                 <AlertCircleIcon className="size-6 text-destructive" />
-                <div className="flex flex-col texts-center gap-y-2">
+                <div className="flex flex-col text-center gap-y-2">
                     <h6 className="text-lg font-medium">{title}</h6>
                     <p className="text-sm ">{description}</p>
                 </div>
